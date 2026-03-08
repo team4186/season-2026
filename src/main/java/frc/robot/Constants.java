9614c555724a8daba97aed7a2cf88917f92f856a
Alameda Aztechs <=>
2026-03-07 18:20:11 -0800

    Further IntakeSUbstem changes. inital implentation of a close loop controller - Shing

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -137,7 +137,8 @@ public final class Constants {
         public static final int INTAKE_RETRACTED_LSChannel2 = 0;
 
         // NEO 550
-        public static final int INTAKE_CAN_ID = 0; //TODO: set values here
+        public static final int INTAKE_STARBOARD_CAN_ID = 0;
+        public static final int INTAKE_PORT_CAN_ID = 0;//TODO: set values here
         public static final int INTAKE_CURRENT_LIMIT = 50;
         public static final double INTAKE_FREE_SPEED = 11000;
         public static final SparkBaseConfig.IdleMode IDLE_MODE = SparkBaseConfig.IdleMode.kBrake;
