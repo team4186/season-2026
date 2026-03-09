7b6fc5182b91c546f064e7de197bb4196ac8231a
Plantainbread <zyuanboost2026@gmail.com>
2026-03-08 18:06:18 -0700

    Implement basic climb subsystem functions - Rishab & Ziyao

---
+++ src/main/java/frc/robot/commands/turretcommands/AutoTurretTargetting.java
@@ -0,0 +1,7 @@
+package frc.robot.commands.turretcommands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class AutoTurretTargetting extends Command {
+
+}
--- src/main/java/frc/robot/subsystems/ClimbSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimbSubsystem.java
@@ -1,10 +1,22 @@
 package frc.robot.subsystems;
 
 
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.ClosedLoopSlot;
+import com.revrobotics.spark.SparkBase;
+import com.revrobotics.spark.SparkClosedLoopController;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.spark.SparkMax;
+import frc.robot.Constants.ClimbConstants;
 
 
 public class ClimbSubsystem extends SubsystemBase {
+    private final SparkMax climbMotor;
+    private final DigitalInput homeSwitch;
+    private final RelativeEncoder climbEncoder;
+    private final SparkClosedLoopController climbMotorController;
     // left climb motor
     // right climb motor
     // pigeon/gyro --- swerve for gyro instance periodically
@@ -13,27 +25,43 @@ public class ClimbSubsystem extends SubsystemBase {
 
 
     public ClimbSubsystem(
-            int endOfTravelConst
+            SparkMax climbMotor,
+            DigitalInput homeSwitch
     ) {
-        this.endOfTravel = endOfTravelConst;
+    this.climbMotor = climbMotor;
+    this.homeSwitch = homeSwitch;
+    this.climbEncoder = climbMotor.getEncoder();
+    this.climbMotorController = climbMotor.getClosedLoopController();
     }
 
 
     @Override
-    public void periodic(){}
+    public void periodic(){
+        SmartDashboard.putBoolean("Climb Limit Switch: ", getLimitSwitch());
+        SmartDashboard.putNumber("Climb Encoder Readings: ", readEncoder());
+        SmartDashboard.putBoolean("Is climb at set point: ", climbMotorController.isAtSetpoint());
+    }
 
 
-    public void climbUp(){}
+    // Generic updateClimb function, setpoint can be some angle where the arm needs to be deployed.
+    // Setpoint could also be some angle where the arm is clamped down.
+    public void updateClimb(double angleSetpoint){
+        climbMotorController.setSetpoint(angleSetpoint, SparkBase.ControlType.kPosition, ClosedLoopSlot.kSlot0);
+    }
 
+    public void climbStop(){
+        climbMotor.stopMotor();
+    }
 
-    public void climbDown(){}
+    public double readEncoder(){
+        return climbEncoder.getPosition();
+    }
 
+    public void resetEncoder(){
+        climbEncoder.setPosition(0);
+    }
 
-    public void climbHold(){}
-
-
-    public void climbStop(){}
-
-
-    public void climbReset(){}
+    public boolean getLimitSwitch(){
+        return homeSwitch.get();
+    }
 }
