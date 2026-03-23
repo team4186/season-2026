b09d965edd89f9157a4efc0f86b565c57ab03a9d
Alameda Aztechs <=>
2026-03-22 19:08:30 -0700

    Updated Predicted max speed values

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -33,7 +33,7 @@ public final class Constants {
     public static final double ROBOT_MASS = (109.2) * 0.453592; // 32lbs * kg per pound
     public static final Matter CHASSIS = new Matter(new Translation3d(0, 0, Units.inchesToMeters(8)), ROBOT_MASS);
     public static final double LOOP_TIME = 0.13; // s, 20ms + 110ms sprk max velocity lag // TODO: Modify?
-    public static final double MAX_SPEED = Units.feetToMeters(18.0); //orig value 14.5
+    public static final double MAX_SPEED = Units.feetToMeters(13.76); //orig value 14.5
     public static final double NOMINAL_VOLTAGE = 12.0;
 
     // Improving Velocity Based Control
